8487a7baaaaf28fda609d80bdecb930115b8a9eb
IncompleteAtom786 <taherkapadia786@gmail.com>
2024-11-08 15:10:10 +0530

    Commit

--- Frontend/src/components/Charts.tsx
+++ Frontend/src/components/Charts.tsx
@@ -4,6 +4,7 @@ import DashedLineChart from "./Charts/LineChart";
 import ScatterPlot from "./Charts/ScatterPlot";
 import StackedBarChart from "./Charts/StackedBar";
 import TwoLevelPieChart from "./Charts/TwoLevelPieChart";
+import PieChartWithCenterLabel from "./Charts/TwoLevelPieChart";
 
 export default function Charts() {
     return <Box sx={{display: "flex", flexDirection: "row", flexWrap: "wrap", justifyItems: "center", alignItems: "center", width: "100", padding: 1}}>
@@ -19,8 +20,8 @@ export default function Charts() {
             <StackedBarChart />
         </Item>
 
-        <Item sx={{ border: "3px solid grey", margin: 2 }}>
-            <TwoLevelPieChart />
+        <Item sx={{ border: "3px solid grey", margin: 2, height: "fit-content"}}>
+            <PieChartWithCenterLabel />
         </Item>
 
         <Item sx={{ border: "3px solid grey", margin: 2 }}>
